4a39d219548b7ff81fbb545d963a1deb3d54d00c
Hou, Changjun <changjun.hou@sap.com>
2017-10-10 14:55:49 +0800

    initial commit

---
+++ src/main/java/com/test/eclipselink/Todo.java
@@ -0,0 +1,42 @@
+package com.test.eclipselink;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private String summary;
+
+	@Column
+	private String description;
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Todo [summary=" + summary + ", description=" + description + "]";
+	}
+
+}
